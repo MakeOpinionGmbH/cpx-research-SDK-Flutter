3a9054d33578004c665d22eeb2cd5f921b56bd17
Dennis <crabby94@outlook.de>
2022-01-17 00:12:41 +0100

    update version to 0.4.0+5

--- lib/main.dart
+++ lib/main.dart
@@ -132,7 +132,7 @@ class _MyHomePageState extends State<MyHomePage> {
               'Flutter SDK Demo App',
               style: TextStyle(fontSize: 23, fontWeight: FontWeight.bold),
             ),
-            Text('Version 0.3.1'),
+            Text('Version 0.4.0'),
             SizedBox(height: 20),
             CPXSurveyCards(),
             ElevatedButton(
